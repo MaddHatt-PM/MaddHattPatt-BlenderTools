fc93d474672019db01411b95f4b469ae5d22b376
MaddHatt-PM <MaddHatt.pm@gmail.com>
2021-09-05 19:29:47 -0400

    added bl_info

--- Learning Playground/aztec_incremental_save.py
+++ Learning Playground/aztec_incremental_save.py
@@ -1,53 +1,43 @@
 import bpy
 import datetime
 import os
+from bpy.app.handlers import persistent
 
-# Idea Flow
-# 0. Always active on save(). Maybe with some event handler if Python has those?
-# 1. Get file name
-# 2. If a folder with "file name" + " Incremental Save", does not exist, create it
-# 3. Save the same version with as "file name_Aug29_22H20M
-#
-# Other things:
-# I don't want to create .backupblend files 
+bl_info = {
+    "version": (0, 1),
+    "blender": (2, 93, 0),
+    "name": "Incremental save to subfolder",
+    "author": "Patt @MaddHattPatt",
+    "description": "Saves files to subfolder with the name of the original blend file",
+    "category": "System"
+    }
 
+@persistent
+def backup_to_subfolder(dummy):
+    filepath = bpy.data.filepath
+    filename = filepath.split('\\')[-1]
 
+    # backup_filename preparation
+    now = datetime.datetime.now()
+    month_id = {1: "JAN", 2: "FEB", 3: "MAR", 4: "APR", 5: "MAY", 6: "JUN", 7: "JUL", 8: "AUG", 9: "SEP", 0: "OCT", 1: "NOV", 2: "DEC"}
+    time_suffix = "_" + month_id[now.month] + str(now.day).zfill(2) + "_" + str(now.hour).zfill(2) + '-' + str(now.minute).zfill(2)
+    backup_filename = filename.replace(".blend", time_suffix + ".blend")
 
-# Save as main file but does it work like "save as copy" 
-# bpy.ops.wm.save_as_mainfile()
+    savefolder = filepath.replace(".blend", "") + '\\'
+    if not os.path.exists(savefolder):
+        os.makedirs(savefolder)
 
-# Gets the filepath of the .blend file
-# bpy.data.filepath
+    if not os.path.exists(savefolder + backup_filename):
+        bpy.ops.wm.save_as_mainfile(filepath=savefolder + backup_filename, copy=True)
 
-# Actives after saving is complete... need example since handlers are new to me with Python
-# https://docs.blender.org/api/current/bpy.app.handlers.html
-# bpy.app.handlers.save_post
+# ------------------------------------------
+# -- Addon-Boilerplate --
+# -----------------------
+def register():
+    bpy.app.handlers.save_post.append(backup_to_subfolder)
 
-month_id = {
-    1: "JAN",
-    2: "FEB",
-    3: "MAR",
-    4: "APR",
-    5: "MAY",
-    6: "JUN",
-    7: "JUL",
-    8: "AUG",
-    9: "SEP",
-    10: "OCT",
-    11: "NOV",
-    12: "DEC"
-}
+def unregister():
+    bpy.app.handlers.save_post.remove(backup_to_subfolder)
 
-filepath = bpy.data.filepath
-filename = filepath.split('\\')[-1]
-
-now = datetime.datetime.now()
-time_suffix = "_" + month_id[now.month] + str(now.day).zfill(2) + "_" + str(now.hour).zfill(2) + '-' + str(now.minute).zfill(2)
-backup_filename = filename.replace(".blend", time_suffix + ".blend")
-
-savefolder = filepath.replace(".blend", "") + '\\'
-print(savefolder)
-if not os.path.exists(savefolder):
-    os.makedirs(savefolder)
-
-bpy.ops.wm.save_as_mainfile(filepath=savefolder + backup_filename, copy=True)
+if __name__ == "__main__":
+    register()
