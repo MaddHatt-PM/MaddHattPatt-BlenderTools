f987eb4eddc1fffb14ea4e96912cffd380acf7ab
MaddHattPatt <MaddHatt.pm@gmail.com>
2021-09-07 11:19:57 -0400

    Started work on maddhattpipeline

---
+++ MaddHatt Pipeline/maddhatt_pipeline.py
@@ -0,0 +1,11 @@
+import bpy
+
+bl_info = {
+    "version": (0, 1),
+    "blender": (2, 93, 0),
+    "name": "MaddHatt's Pipeline Tool",
+    "author": "Patt @MaddHattPatt",
+    "description": "Saves files to subfolder with the name of the original blend file",
+    "category": "System"
+    }
+
