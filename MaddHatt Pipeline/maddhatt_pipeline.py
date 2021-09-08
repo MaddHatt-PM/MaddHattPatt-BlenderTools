26072050c4837da79791032fa68f28bb4fdac9fe
MaddHattPatt <MaddHatt.pm@gmail.com>
2021-09-08 14:49:05 -0400

    got boilerplate for appropriate viewing in Blender

--- MaddHatt Pipeline/maddhatt_pipeline.py
+++ MaddHatt Pipeline/maddhatt_pipeline.py
@@ -1,11 +1,32 @@
 import bpy
 
+# Addon info
 bl_info = {
     "version": (0, 1),
-    "blender": (2, 93, 0),
+    "blender": (2, 91, 0),
     "name": "MaddHatt's Pipeline Tool",
-    "author": "Patt @MaddHattPatt",
-    "description": "Saves files to subfolder with the name of the original blend file",
-    "category": "System"
+    "author": "Patt @MaddHattPatt <MaddHatt.pm@gmail.com>",
+    "description": "A variety of tools to help out my personal ",
+    "location": "3D View on the side bar",
+    "category": "3D View"
     }
 
+class MADDHATT_OT_setup_midpoly(bpy.types.Operator):
+    bl_idname = "maddhatt.setup_midpoly"
+    bl_label = "Setup mid poly"
+
+    def execute(self, context):
+        print("maddhatt.setup_from_midpoly_collection was called")
+        return {'FINISHED'}
+
+# Class registration
+reg_classes = [
+    MADDHATT_OT_setup_midpoly]
+
+def register():
+    for item in reg_classes:
+        bpy.utils.register_class(item)
+
+def unregister():
+    for item in reg_classes:
+        bpy.utils.unregister_class(item)
