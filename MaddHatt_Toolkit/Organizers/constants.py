6f15a4c19765270c568dbfe1ce9086fbdc7dde3e
MaddHattPatt <MaddHatt.pm@gmail.com>
2021-11-03 13:04:46 -0400

    added helper method to get suffix

--- MaddHatt_Toolkit/Organizers/constants.py
+++ MaddHatt_Toolkit/Organizers/constants.py
@@ -7,3 +7,13 @@ HIGHPOLY = "High_Poly"
 SUF_LOW = "_low"
 SUF_MID = "_mid"
 SUF_HIGH = "_high"
+
+def coll_to_suffix(input:str):
+    if input == LOWPOLY:
+        return SUF_LOW
+        
+    if input == MIDPOLY:
+        return SUF_MID
+
+    if input == HIGHPOLY:
+        return SUF_HIGH
